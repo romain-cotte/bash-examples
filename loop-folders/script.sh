ce8cad12708dcfcf98db353c2f3d2586213fbfe3
Romain Cotte <romain.cotte@gmail.com>
2020-12-16 18:02:38 +0100

    loop script

--- loop-folders/script.sh
+++ loop-folders/script.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+pwd=$PWD
 cd ~/axa
+echo $pwd
 
-for dir in */ ; do
+for dir in * ; do
   cd $dir
 
   # The code
   if [ -d ".git" ]; then
     ### Take action if $DIR exists ###
-    echo "$dir ok"
-    git config user.email "romain.cotte.meyer@axa.com"
+    # git config user.email "romain.cotte.meyer@axa.com"
+
+    # echo "$dir ok"
+    if [[ -f "MY_README.md" ]]; then
+        name=$(basename $dir)
+        echo "$pwd/${name}_MY_README.md"
+        cp MY_README.md "$pwd/${name}_MY_README.md"
+    fi
   fi
 
   cd $OLDPWD
