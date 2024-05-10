186143719196370963d228cce0add14ca7cc9597
Romain Cotte <romain.cotte@gmail.com>
2024-05-10 11:40:06 +0200

    change loop folders

--- loop-folders/script.sh
+++ loop-folders/script.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 pwd=$PWD
-cd ~/axa
+cd ~/folder
 echo $pwd
 
 for dir in * ; do
-  cd $dir
+  if [ -d "$dir" ]; then
+    cd $dir
+    if [ -d ".git" ]; then
+      ### Take action if $DIR exists ###
+      # git config user.email "youremail@domain.com"
 
-  # The code
-  if [ -d ".git" ]; then
-    ### Take action if $DIR exists ###
-    # git config user.email "romain.cotte.meyer@axa.com"
-
-    # echo "$dir ok"
-    if [[ -f "MY_README.md" ]]; then
-        name=$(basename $dir)
-        echo "$pwd/${name}_MY_README.md"
-        cp MY_README.md "$pwd/${name}_MY_README.md"
+      # echo "$dir ok"
+      if [[ -f "MY_README.md" ]]; then
+          name=$(basename $dir)
+          echo "$pwd/${name}_MY_README.md"
+          cp MY_README.md "$pwd/${name}_MY_README.md"
+      fi
     fi
-  fi
 
-  cd $OLDPWD
+    cd $OLDPWD
+  fi
 done
